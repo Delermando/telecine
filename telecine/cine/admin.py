fabb5f3cd55ecce9f863a174944b70a5b6416188
Delermando <delsantos@hotmail.com.br>
2015-09-13 21:43:06 -0300

    create app movies

--- telecine/cine/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from cine.models import Pessoa
-
-admin.site.register(Pessoa)
--- telecine/cine/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home( request ):
-    context = {'texto': 'Seu primeiro projeto em Django'}
-    return render(request, 'index.html', context)
